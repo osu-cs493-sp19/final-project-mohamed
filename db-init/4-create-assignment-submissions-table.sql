59f795eb6a31f1dbfdd210414d62196e79f0e337
Jason Chen <jason@jcndrop.com>
2019-06-06 14:55:36 -0700

    Add database migrations

---
+++ db-init/4-create-assignment-submissions-table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS assignment_submissions (
+  id SERIAL NOT NULL,
+  assignment_id INT NOT NULL,
+  student_id INT NOT NULL,
+  submitted_at TIMESTAMPTZ NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (assignment_id) REFERENCES assignments (id) ON DELETE CASCADE,
+  FOREIGN KEY (student_id) REFERENCES users (id) ON DELETE CASCADE
+);
