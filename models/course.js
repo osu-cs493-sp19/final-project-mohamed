58ed1f1dd8ab6b8abbae172c402969332bb01e8f
Jason Chen <jason@jcndrop.com>
2019-06-07 22:25:56 -0700

    Change Course model to use Model class

--- models/course.js
+++ models/course.js
@@ -1,25 +1,5 @@
 const { db } = require('../lib/db')
-const { ValidationError } = require('../lib/validation')
 
-const CourseSchema = {
-  subject: { required: true },
-  courseNumber: { required: true },
-  title: { required: true },
-  term: { required: true },
-  instructorId: { required: true }
-}
+class Course extends Model {}
 
-const all = (page, per) => {
-}
-
-const createCourse = (data) => {
-  data = extractSchemaFields(data)
-  const query = {
-    text: 'INSERT INTO courses (subject, course_number, title, term, instructor_id) VALUES ($1, $2, $3, $4, $5) RETURNING id',
-    values: [data.subject, data.courseNumber, data.title, data.term, data.instructorId]
-  }
-
-  return db.query(query)
-    .then(res => res.rows[0].id)
-    .catch(e => e)
-}
+exports.Course = Course
